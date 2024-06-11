53ddbd8f254eb99812335f21777e633cadfcb9ad
emiliano Gillini <emigillini@hotmail.com>
2024-06-11 14:36:02 -0300

    Starting_Proyect_Angular_Config_Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Asegúrate de que Tailwind procese todos los archivos relevantes
+    
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
