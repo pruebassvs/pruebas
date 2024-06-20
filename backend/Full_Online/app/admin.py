1d9778e522e91749558da5c67cf913799bdb1487
emiliano Gillini <emigillini@hotmail.com>
2024-06-20 12:05:19 -0300

    Register models in admin.py and apply migrations

--- backend/Full_Online/app/admin.py
+++ backend/Full_Online/app/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .models import *
 
-# Register your models here.
+@admin.register(ColorType)
+class ColorTypeAdmin(admin.ModelAdmin):
+    list_display = ('description',)
+    ordering = ('description',)
+
+@admin.register(BrandType)
+class BrandTypeAdmin(admin.ModelAdmin):
+    list_display = ('description',)
+    ordering = ('description',)
+
+@admin.register(SizeType)
+class SizeTypeAdmin(admin.ModelAdmin):
+    list_display = ('size',)
+    ordering = ('size',)
+
+@admin.register(ShoeModelType)
+class ShoeModelTypeAdmin(admin.ModelAdmin):
+    list_display = ('model',)
+    ordering = ('model',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'color', 'size', 'price', 'stock')
+    list_filter = ('brand', 'color', 'size')
+    search_fields = ('model__model', 'brand__description', 'color__description', 'size__size')
+    ordering = ('model', 'brand', 'color', 'size')
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date')
+    search_fields = ('user__email', 'date')
+    ordering = ('-date',)  
+
+@admin.register(CartDetail)
+class CartDetailAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
+    list_filter = ('cart__user', 'product__model')
+    search_fields = ('cart__user__email', 'product__model__model')
+    ordering = ('cart', 'product')
+
+@admin.register(PaymentModeType)
+class PaymentModeTypeAdmin(admin.ModelAdmin):
+    list_display = ('description',)
+    ordering = ('description',)
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'date', 'user', 'total', 'payment_type')
+    list_filter = ('date', 'user', 'payment_type')
+    search_fields = ('invoice_number', 'user__email')
+    ordering = ('-date',)  
+
+@admin.register(PurchaseDetail)
+class PurchaseDetailAdmin(admin.ModelAdmin):
+    list_display = ('purchase', 'product', 'quantity', 'price')
+    list_filter = ('purchase__user', 'product__model')
+    search_fields = ('purchase__user__email', 'product__model__model')
+    ordering = ('purchase', 'product')
+
+@admin.register(DeliveryStatusType)
+class DeliveryStatusTypeAdmin(admin.ModelAdmin):
+    list_display = ('description',)
+    list_filter = ('description',)
+    ordering = ('description',)
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ('purchase', 'tracking_number', 'delivery_address', 'estimated_date', 'delivery_date', 'delivery_status')
+    list_filter = ('delivery_status', 'estimated_date', 'delivery_date')
+    search_fields = ('tracking_number', 'purchase__invoice_number')
+    ordering = ('-estimated_date',)  
+
+@admin.register(get_user_model())
+class UserAdmin(UserAdmin):
+    pass
+
